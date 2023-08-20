c176dc3bb760fea8a7c0b0c656aa28671cd04b99
Muhammad14325 <117213362+Muhammad14325@users.noreply.github.com>
2023-08-20 12:51:27 +0100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+// Create function addUser()
+function addUser{
+  // Get value of user by id player1_name_input and player2_name_input
+id="player1_name_input player2_name_input"
+
+  //Assign "game_page.html" to window.location
+  window.location="game_page.html"
+}
+
